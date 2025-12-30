3af56a4480b23c924351ce017dc1bb88d2c2d69d
JSY-Ben <cheesypz@hotmail.com>
2025-12-30 21:00:47 +0000

    Move back CREATE EXAM button to top

--- staff/index.php
+++ staff/index.php
@@ -133,6 +133,10 @@ $pageScripts = '<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/
 require __DIR__ . '/../header.php';
 ?>
 <main class="container py-4">
+    <div class="d-flex justify-content-end mb-3">
+        <a class="btn btn-primary btn-lg" href="create_exam.php">Create Exam</a>
+    </div>
+
     <div class="card shadow-sm mb-4">
         <div class="card-body">
             <div class="accordion" id="filterAccordion">
@@ -203,7 +207,6 @@ require __DIR__ . '/../header.php';
 
     <div class="d-flex justify-content-between align-items-center mb-3">
         <h1 class="h4 mb-0">Exams</h1>
-        <a class="btn btn-primary btn-lg" href="create_exam.php">Create Exam</a>
     </div>
 
     <div class="card shadow-sm">
