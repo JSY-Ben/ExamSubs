295661eb452fa59fe0539e1e9f341690193c05e5
JSY-Ben <cheesypz@hotmail.com>
2026-01-01 23:04:31 +0000

    Download Exam Files button added on homepage

--- index.php
+++ index.php
@@ -24,6 +24,23 @@ foreach ($stmt->fetchAll() as $exam) {
         $exams[] = $exam;
     }
 }
+
+$examFilesByExam = [];
+if (count($exams) > 0) {
+    $examIds = array_map(static function (array $exam): int {
+        return (int) $exam['id'];
+    }, $exams);
+    $placeholders = implode(',', array_fill(0, count($examIds), '?'));
+    $stmt = db()->prepare("SELECT * FROM exam_files WHERE exam_id IN ($placeholders) ORDER BY uploaded_at DESC, id DESC");
+    $stmt->execute($examIds);
+    foreach ($stmt->fetchAll() as $file) {
+        $examId = (int) $file['exam_id'];
+        if (!isset($examFilesByExam[$examId])) {
+            $examFilesByExam[$examId] = [];
+        }
+        $examFilesByExam[$examId][] = $file;
+    }
+}
 $pageTitle = 'Exams Submission Portal';
 $brandHref = 'index.php';
 $brandText = 'Exams Submission Portal';
@@ -50,9 +67,35 @@ require __DIR__ . '/header.php';
                             <p class="text-muted mb-3">
                                 Window: <?php echo e(format_datetime_display($exam['start_time'])); ?> to <?php echo e(format_datetime_display($exam['end_time'])); ?>
                             </p>
-                            <a class="btn btn-primary" href="student_exam.php?id=<?php echo (int) $exam['id']; ?>">
-                                Submit Files
-                            </a>
+                            <div class="d-flex flex-wrap gap-2">
+                                <a class="btn btn-primary" href="student_exam.php?id=<?php echo (int) $exam['id']; ?>">
+                                    Submit Files
+                                </a>
+                                <?php
+                                $examId = (int) $exam['id'];
+                                $examFiles = $examFilesByExam[$examId] ?? [];
+                                ?>
+                                <?php if (count($examFiles) > 0): ?>
+                                    <div class="dropdown">
+                                        <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                            Download Exam Files
+                                        </button>
+                                        <ul class="dropdown-menu">
+                                            <li>
+                                                <a class="dropdown-item" href="download_exam_files_zip.php?exam_id=<?php echo $examId; ?>">Download all (ZIP)</a>
+                                            </li>
+                                            <li><hr class="dropdown-divider"></li>
+                                            <?php foreach ($examFiles as $file): ?>
+                                                <li>
+                                                    <a class="dropdown-item" href="download_exam_file.php?id=<?php echo (int) $file['id']; ?>">
+                                                        <?php echo e($file['title'] !== '' ? $file['title'] : $file['original_name']); ?>
+                                                    </a>
+                                                </li>
+                                            <?php endforeach; ?>
+                                        </ul>
+                                    </div>
+                                <?php endif; ?>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -60,4 +103,5 @@ require __DIR__ . '/header.php';
         </div>
     <?php endif; ?>
 </main>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 <?php require __DIR__ . '/footer.php'; ?>
